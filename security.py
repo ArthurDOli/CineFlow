332b345301fbddb50a2a5334a405df805af79aee
Arthur Duarte Oliveira <arthur.duarte.oli06@gmail.com>
2025-09-29 16:44:47 -0300

    Login and account creation

--- security.py
+++ security.py
@@ -2,12 +2,27 @@ from dotenv import load_dotenv
 import os
 from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordBearer
+from datetime import datetime, timedelta
+from jose import jwt
 
 load_dotenv()
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 ALGORITHM = os.getenv('ALGORITHM')
-ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES')
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES'))
 
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
-# oauth2_schema = OAuth2PasswordBearer(tokenUrl='auth/login')
+oauth2_schema = OAuth2PasswordBearer(tokenUrl='auth/login')
+
+def verify_password(plain_password, hashed_password):
+    return bcrypt_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password):
+    return bcrypt_context.hash(password)
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({'exp': expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
